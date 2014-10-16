cf69e140ff3afd761d06eaa41d58e9305010a4bb
Tim Sandberg <tasandberg@gmail.com>
2014-10-16 08:50:15 -0700

    Colors adjusted

--- board.rb
+++ board.rb
@@ -1,14 +1,14 @@
 class Board
   attr_reader :grid
-  
+
   class IllegalMoveError < StandardError; end
-  
+
   def initialize(empty_board = false)
       @grid = Array.new(8) { Array.new(8) }
       set_board(@grid) unless empty_board
       @captured_pieces = []
   end
-  
+
   def in_check?(color)
     king = king_pos(color)
     check = false
@@ -21,7 +21,7 @@ class Board
 
     check
   end
-  
+
   def king_pos(color)
     @grid.flatten.each do |piece|
       if piece && piece.is_a?(King) && piece.color == color
@@ -29,10 +29,10 @@ class Board
       end
     end
   end
-  
+
   def display
     puts "CHESS".center(28)
-    
+
     @grid.each_with_index do |row, i|
       row_str = "#{8 - i} "
       row.each_with_index do |piece, j|
@@ -41,35 +41,35 @@ class Board
       end
       puts row_str
     end
-    
+
     footer = ' '
     ('A'..'H').each { |letter| footer += "\e[36m#{letter.rjust(3)}\e[0m" }
     puts footer
-    
+
     print_captured_pieces
   end
-  
+
   def set_board(matrix)
     [:black, :white].each do |color|
       create_pawn_row(color, matrix)
       create_back_row(color, matrix)
     end
   end
-  
+
   def create_pawn_row(color, matrix)
     row = color == :black ? 1 : 6
     pawns = []
     (0..7).each do |i|
       pawns << Pawn.new([row, i], color, self)
     end
-    
+
     matrix[row] = pawns
   end
-  
+
   def create_back_row(color, matrix)
-    
+
     row = color == :black ? 0 : 7
-    
+
     new_pieces = [
       Rook.new([row, 0], color, self),
       Knight.new([row, 1], color, self),
@@ -80,16 +80,16 @@ class Board
       Knight.new([row, 6], color, self),
       Rook.new([row, 7], color, self),
     ]
-    
+
     matrix[row] = new_pieces
   end
-  
 
-  
+
+
   def move(pos1, pos2)
     piece = self[pos1]
     target = self[pos2]
-    
+
     begin
       raise IllegalMoveError.new("Can't move empty place") if !piece
       moves = piece.valid_moves
@@ -105,15 +105,15 @@ class Board
       puts "IllegalMoveError: #{e}"
       return false
     end
-    
+
   end
-  
+
   def capture_piece(piece, target)
     self[target.pos] = piece
     @captured_pieces << target
     self[piece.pos] = nil
   end
-  
+
   def print_captured_pieces
     captured_str = "Captured: "
     @captured_pieces.each {|piece| captured_str += piece.to_s.rjust(3)}
@@ -124,10 +124,10 @@ class Board
       next if piece.color != color
       return false unless piece.valid_moves.empty?
     end
-    
+
     true
   end
-  
+
   # moving for tests
   def move!(pos1, pos2)
     begin
@@ -136,18 +136,18 @@ class Board
       moves = piece.moves
       target = self[pos2]
       raise IllegalMoveError.new("Illegal move")if !moves.include?(pos2)
-      
-        
+
+
       self[pos1], self[pos2] = self[pos2], self[pos1]
       piece.update_pos
-    
+
     rescue IllegalMoveError => e
       puts "IllegalMoveError: #{e}"
     end
-  
+
   end
-  
-  
+
+
   def find(piece)
     @grid.each_with_index  do |row, i|
       row.each_with_index { |col, j| return [i, j] if col == piece }
@@ -165,26 +165,26 @@ class Board
 
     dup_board
   end
-  
+
   def [](pos)
     row, col = pos[0],pos[1]
-    
+
     @grid[row][col]
   end
-  
+
   def []=(pos, piece)
     row, col = pos[0],pos[1]
     @grid[row][col] = piece
   end
-  
+
   def background(pos, string)
     background = ""
     if pos[0].even?
-      background = pos[1].even? ? "\e[47m" : "\e[37m"
+      background = pos[1].even? ? "\e[1;100m" : "\e[47m"
     else
-      background = pos[1].odd? ? "\e[47m" : "\e[37m"
+      background = pos[1].odd? ? "\e[1;100m" : "\e[47m"
     end
     background + string
   end
-  
+
 end
--- piece.rb
+++ piece.rb
@@ -1,51 +1,51 @@
 class Piece
   attr_reader :pos, :color, :board
   attr_writer :pos
-  
+
 
   class NotImplementedError < StandardError; end
-  
+
   def initialize(pos, color, board)
     @pos   = pos
     @color = color
     @board = board
   end
-  
+
   def moves
     raise NotImplementedError.new("Moves method not implemented")
   end
-  
+
   def update_pos
     @pos = @board.find(self)
   end
-  
-  
+
+
   def on_board?(pos)
     row, col = pos
     row.between?(0,7) && col.between?(0,7)
   end
-  
+
   def valid_move?(pos)
-    on_board?(pos) && (is_enemy?(pos) || @board[pos].nil?) 
+    on_board?(pos) && (is_enemy?(pos) || @board[pos].nil?)
   end
-  
+
   def is_enemy?(pos)
     !@board[pos].nil? && @board[pos].color != @color
   end
-  
+
   def move_into_check?(new_pos)
     dup_board = @board.dup
     dup_board.move!(@pos, new_pos)
     dup_board.in_check?(@color)
   end
-  
+
   def valid_moves
     self.moves.select {|move| move unless move_into_check?(move)}
   end
-  
+
   def colorize(string)
-    color = @color == :black ? "\e[30m" : "\e[37m"
-    color + string + "\e[0m"
+    color = @color == :black ? "\e[30m" : "\e[1;97m"
+    color + string.center(3) + "\e[0m"
   end
 
 end
